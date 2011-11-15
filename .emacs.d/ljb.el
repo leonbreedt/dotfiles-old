1f28e56933c8ad7e079cfa92cf970f180f93f24c
Leon Breedt <ljb@bitserf.org>
2011-11-15 23:33:12 +1300

    Fix up path for ruby-compilation to use rbenv Ruby, and not system Ruby.

--- .emacs.d/ljb.el
+++ .emacs.d/ljb.el
@@ -32,3 +32,18 @@
 
 ;; Start server by default
 (server-start)
+
+;; Never use tabs, only spaces, and 4 of them
+(setq indent-tabs-mode nil)
+(setq c-basic-offset 4)
+(setq tab-width 4)
+
+;; Custom PATH for subprocesses
+(if (equal system-type 'darwin)
+    (progn
+      (setenv "PATH" (concat "/usr/local/bin:/usr/local/sbin:/Users/ljb/.rbenv/bin:/Users/ljb/.rbenv/shims:" (getenv "PATH")))
+      (setq exec-path (append '("/usr/local/bin"
+                                "/usr/local/sbin"
+                                "~/.rbenv/bin"
+                                "~/.rbenv/shims") exec-path))))
+
--- .emacs.d/starter-kit-ruby.el
+++ .emacs.d/starter-kit-ruby.el
@@ -10,7 +10,8 @@
      (add-hook 'ruby-mode-hook 'inf-ruby-keys)
      (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
      (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
-     (define-key ruby-mode-map (kbd "C-c l") "lambda")))
+     (define-key ruby-mode-map (kbd "C-c l") "lambda")
+     (define-key ruby-mode-map (kbd "C-c c") 'ruby-compilation-this-buffer)))
 
 (global-set-key (kbd "C-h r") 'ri)
 
