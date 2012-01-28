90b87d5b1fc4ad3e28eb1036d446d332aa2e4fdd
Leon Breedt <ljb@bitserf.org>
2012-01-28 16:12:52 +1300

    Use $LOCAL.

--- bin/setup-dotfiles.sh
+++ bin/setup-dotfiles.sh
@@ -5,10 +5,10 @@ set -e
 REMOTE=git://github.com/bitserf/dotfiles.git
 LOCAL=$HOME/.dotfiles
 
-if [ -e "$HOME/.dotfiles" ]; then
-  git clone $REMOTE $LOCAL
-else
+if [ -e $LOCAL ]; then
   git --git-dir=$LOCAL pull origin master
+else
+  git clone $REMOTE $LOCAL
 fi
 ruby $LOCAL/bin/link-dotfiles.rb
 git --git-dir=$LOCAL submodule update --init
