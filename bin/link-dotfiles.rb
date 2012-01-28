303ff951f1c8004ac14af0ae98451818634dc696
Leon Breedt <ljb@bitserf.org>
2012-01-28 16:54:39 +1300

    Unnecessary.

--- bin/link-dotfiles.rb
+++ bin/link-dotfiles.rb
@@ -2,34 +2,15 @@
 # link dotfiles into $HOME
 
 require 'pathname'
-require 'optparse'
-
-options = {}
-parser = OptionParser.new do |opts|
-  opts.banner = "Usage: #{File.basename($0)} [options]"
-  opts.separator "Options:"
-  opts.on("-f", "--force", "Always replace existing files/links")
-end
-parser.parse!
 
 def link(source, dest, force=false)
   if File.symlink?(dest)
     target = File.readlink(dest)
-    if target == source
-      return
-    else
-      puts "removing #{dest} (symlink)"
-      File.unlink(dest)
-    end
-  elsif File.exists?(dest)
-    if force && File.file?(dest)
-      puts "removing #{dest} (forced)"
-      File.unlink(dest)
-    end
+    return if target == source
+    puts "removing #{dest} (symlink)"
+    File.unlink(dest)
   end
-  if File.exists?(dest)
-    abort("error: #{dest} already exists")
-  end
+  abort("error: #{dest} already exists") if File.exists?(dest)
   File.symlink(source, dest)
   puts "linked #{dest}"
 end
