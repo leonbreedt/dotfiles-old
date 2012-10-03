d811e2f4001e319dfaa7e66a5bf13d3cca0112e2
Leon Breedt <ljb@bitserf.org>
2012-10-03 19:43:58 +1300

    On OS X, install homebrew kegs as part of setup.

--- bin/setup-dotfiles.sh
+++ bin/setup-dotfiles.sh
@@ -14,3 +14,25 @@ else
 fi
 ruby bin/link-dotfiles.rb
 git submodule update --init
+
+# OS X only
+function install_keg()
+{
+  keg="$1"
+  shift
+  args="$*"
+  if brew info $keg | grep 'Not installed' >/dev/null; then
+    cmd="brew install $keg $args"
+    echo $cmd
+    brew install $keg $args;
+  else
+    echo "$keg already installed"
+  fi
+}
+
+which brew && {
+  for keg in msmtp gnupg contacts clojure scala sbt rbenv ruby-build urlview tree xz emacs notmuch watch; do 
+    install_keg $keg
+  done
+  install_keg mutt --with-slang
+}
